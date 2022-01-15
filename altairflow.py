87b5fa38ebcae38657e43c51fd226737955f610a
Hamel Husain <hamel@m1.local>
2022-01-14 20:34:28 -0800

    save

---
+++ altairflow.py
@@ -0,0 +1,73 @@
+import math, random
+from metaflow import step, FlowSpec, card, conda
+
+class AltairFlow(FlowSpec):
+
+    @card(type='altair')
+    @conda(libraries={"altair": "4.2.0", "vega_datasets": "0.9.0"})
+    @step
+    def start(self):
+        import altair as alt
+        from vega_datasets import data
+
+        source = data.seattle_weather()
+
+        scale = alt.Scale(domain=['sun', 'fog', 'drizzle', 'rain', 'snow'],
+                        range=['#e7ba52', '#a7a7a7', '#aec7e8', '#1f77b4', '#9467bd'])
+        color = alt.Color('weather:N', scale=scale)
+
+        # We create two selections:
+        # - a brush that is active on the top panel
+        # - a multi-click that is active on the bottom panel
+        brush = alt.selection_interval(encodings=['x'])
+        click = alt.selection_multi(encodings=['color'])
+
+        # Top panel is scatter plot of temperature vs time
+        points = alt.Chart().mark_point().encode(
+            alt.X('monthdate(date):T', title='Date'),
+            alt.Y('temp_max:Q',
+                title='Maximum Daily Temperature (C)',
+                scale=alt.Scale(domain=[-5, 40])
+            ),
+            color=alt.condition(brush, color, alt.value('lightgray')),
+            size=alt.Size('precipitation:Q', scale=alt.Scale(range=[5, 200]))
+        ).properties(
+            width=550,
+            height=300
+        ).add_selection(
+            brush
+        ).transform_filter(
+            click
+        )
+
+        # Bottom panel is a bar chart of weather type
+        bars = alt.Chart().mark_bar().encode(
+            x='count()',
+            y='weather:N',
+            color=alt.condition(click, color, alt.value('lightgray')),
+        ).transform_filter(
+            brush
+        ).properties(
+            width=550,
+        ).add_selection(
+            click
+        )
+
+        chart = alt.vconcat(
+            points,
+            bars,
+            data=source,
+            title="Seattle Weather: 2012-2015"
+        )
+
+        chart.save('chart.html')
+        with open('chart.html', 'r') as f:
+            self.chart = f.read()
+
+        self.next(self.end)
+    @step
+    def end(self):
+        pass
+
+if __name__ == '__main__':
+    AltairFlow()
---
+++ metaflow_ext/outerbounds/plugins/cards/uplot-altair/__init__.py
@@ -0,0 +1,22 @@
+import os, json
+from metaflow.cards import MetaflowCard
+from metaflow.plugins.cards.card_modules import chevron
+
+class AltairCard(MetaflowCard):
+    type = "Altair"
+
+    def _read_template(self):
+        root = os.path.join(os.path.dirname(__file__), 'altair')
+        tmpl = {}
+        for key, fname in [('card', 'card.html')]:            
+            with open(os.path.join(root, fname)) as f:
+                tmpl[key] = f.read()
+        return tmpl
+
+    def render(self, task):
+        tmpl = self._read_template()
+        chart_html = task.data.chart
+        tmpl['chart'] = chart_html
+        return chevron.render(tmpl['card'], tmpl)
+
+CARDS = [AltairCard]
